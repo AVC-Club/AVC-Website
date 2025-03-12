87a46408344d8ab9ac944f85787cd83be0743e6e
Justin San. <santosojustin@gmail.com>
2025-03-12 18:18:18 +1100

    implement navbar, header, and footer

--- template/navbar.temp.php
+++ template/navbar.temp.php
@@ -1,6 +1,36 @@
-<a href="/">Home</a>
-<a href="/fixtures">Fixtures</a>
-<a href="/gallery">Gallery</a>
-<a href="/ourTeam">Our Team</a>
-<a href="/committee">Committee</a>
-<a href="/aboutUs">About Us</a>
+<!--START OF NAVBAR-->
+
+<body class="d-flex flex-column min-vh-100">
+    <nav class="navbar navbar-dark bg-dark navbar-expand-md sticky-top py-3" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="/">
+                <a class="navbar-brand" href="https://www.facebook.com/groups/7061129270619986">
+                    <img src="public/src/images/AVC-Logo.png" alt="AVC Logo" width="64" height="64">
+                    Alliance Volleyball Club
+                </a>
+            </a>
+            <!--MOBILE BURGER MENU-->
+            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/fixture-results">Fixtures</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/gallery">Gallery</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/teams">Our Teams</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/committee">Committee</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/about-us">About Us</a></li>
+                </ul>
+                <a class="btn btn-primary shadow mb-3 mt-3" role="button" href="/open-gyms">Come Play!</a>
+            </div>
+            <a href="https://www.facebook.com/groups/7061129270619986"></a>
+            <a href="https://www.instagram.com/alliancevolleyball.club/"></a>
+        </div>
+    </nav>
+
+    <!--END OF NAVBAR-->
+
+    <main class="flex-grow-1 container text-center">
+    <div class="container-md mt-4">
