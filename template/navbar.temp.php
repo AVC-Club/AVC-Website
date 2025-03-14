f12f9916acb79c54219b10aa8b9543f7f89ae46e
Justin San. <santosojustin@gmail.com>
2025-03-14 22:17:00 +1100

    Fix navbar for mobile

--- template/navbar.temp.php
+++ template/navbar.temp.php
@@ -1,20 +1,24 @@
 <!--START OF NAVBAR-->
 
 <body class="d-flex flex-column min-vh-100">
-    <nav class="navbar navbar-dark bg-dark navbar-expand-md sticky-top py-3" id="mainNav">
-        <div class="container">
-            <a class="navbar-brand d-flex align-items-center" href="/">
-                <a class="navbar-brand" href="https://www.facebook.com/groups/7061129270619986">
-                    <img src="/favicon.ico" alt="AVC Logo" width="64" height="64">
-                    Alliance Volleyball Club
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg sticky-top py-3" id="mainNav">
+        <div class="container d-flex align-items-center justify-content-between border-bottom">
+
+            <!-- Logo & Title -->
+            <div class="d-flex align-items-center">
+                <a class="navbar-brand d-flex align-items-center" href="https://www.facebook.com/groups/7061129270619986">
+                    <img src="/favicon.ico" alt="AVC Logo" width="64" height="64" class="me-2">
+                    <span>Alliance Volleyball Club</span>
                 </a>
-            </a>
-            <!--MOBILE BURGER MENU-->
-            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span>
+            </div>
+
+            <!-- Burger Menu -->
+            <button class="navbar-toggler ms-auto d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navcol-menu" aria-controls="navcol-menu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navcol-1">
+            <!-- Navbar Items -->
+            <div class="collapse navbar-collapse text-center" id="navcol-menu">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="/fixture-results">Fixtures</a></li>
@@ -25,12 +29,10 @@
                 </ul>
                 <a class="btn btn-primary shadow mb-3 mt-3" role="button" href="/open-gyms">Come Play!</a>
             </div>
-            <a href="https://www.facebook.com/groups/7061129270619986"></a>
-            <a href="https://www.instagram.com/alliancevolleyball.club/"></a>
         </div>
     </nav>
 
     <!--END OF NAVBAR-->
 
     <main class="flex-grow-1 container text-center">
-    <div class="container-md mt-4">
+        <div class="container-md mt-4 pt-2 pb-2">
