01ba951e28527a8733fd9a41cf27459f9af16f7e
Justin San. <santosojustin@gmail.com>
2025-03-14 22:17:19 +1100

    more cleaning up

--- template/header.temp.php
+++ template/header.temp.php
@@ -1,9 +1,9 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php basename($_SERVER['PHP_SELF'], ".php") ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
