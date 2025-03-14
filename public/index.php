fa9dc271063aa42f7ea5ffa3b4350d273bbb0639
Justin San. <santosojustin@gmail.com>
2025-03-14 22:17:45 +1100

    implementing content for index page

--- public/index.php
+++ public/index.php
@@ -3,7 +3,26 @@ require(__DIR__ . '/../template/header.temp.php');
 require(__DIR__ . '/../template/navbar.temp.php');
 ?>
 
-<h1>INDEX</h1>
+    <div class="row">
+        <div class="col-md-8 col-xl-3 text-center text-md-start mx-auto">
+            <section class="py-4 py-xl-5">
+                <div class="text-center p-4 p-lg-5">
+                    <p class="fw-bold text-primary mb-2">
+                        <span class="text-secondary">Proud to introduce</span>
+                    </p>
+                    <h1 class="fw-bold mb-4 text-white">Alliance Volleyball Club</h1>
+                    <a href="/open-gyms.html" class="btn btn-primary fs-5 me-2 py-2 px-4">
+                        Come Play!
+                    </a>
+                </div>
+            </section>
+        </div>
+        <div class="col">
+            <!-- IMAGES GOES HERE -->
+        </div>
+    </div>
+
+
 
 <?php
 require(__DIR__ . '/../template/footer.temp.php');
