2d5c64b2e4867feca5cb699c29886157d4cccb6b
Justin San. <santosojustin@gmail.com>
2025-03-16 11:27:01 +1100

    Create about-us.php

---
+++ public/about-us.php
@@ -0,0 +1,11 @@
+<?php
+include_once(__DIR__ . '/../component/card.comp.php');
+
+require(__DIR__ . '/../template/header.temp.php');
+require(__DIR__ . '/../template/navbar.temp.php');
+?>
+
+
+<?php
+require(__DIR__ . '/../template/footer.temp.php');
+?>
