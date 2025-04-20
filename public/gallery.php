269f5c36ce21ac95f594d655048a11daa0ff0d43
Justin San. <santosojustin@gmail.com>
2025-04-20 12:08:02 +1000

    Got the basic gallery module to work

--- public/gallery.php
+++ public/gallery.php
@@ -7,6 +7,40 @@ require(__DIR__ . '/../template/navbar.temp.php');
 <h1 class="text-primary">Memories</h1>
 <p class="text-muted pb-4">Explore stunning photos taken by talented photographers during our State League journey</p>
 
+<div class="owl-carousel owl-theme">
+    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
+    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
+    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
+    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
+    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
+    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
+    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
+    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
+
+</div>
+
+<script>
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+</script>
+
+
+
+
 <?php
 require(__DIR__ . '/../template/footer.temp.php');
 ?>
--- template/header.temp.php
+++ template/header.temp.php
@@ -5,6 +5,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php basename($_SERVER['PHP_SELF'], ".php") ?></title>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="/css/AVC-Theme-BS5.override.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
