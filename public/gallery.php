6d2b23372482e3120ce0399ba687d018498ffd0d
Justin San. <santosojustin@gmail.com>
2025-04-20 12:43:51 +1000

    Gallery improvement

--- public/gallery.php
+++ public/gallery.php
@@ -7,22 +7,57 @@ require(__DIR__ . '/../template/navbar.temp.php');
 <h1 class="text-primary">Memories</h1>
 <p class="text-muted pb-4">Explore stunning photos taken by talented photographers during our State League journey</p>
 
+
+
+
 <div class="owl-carousel owl-theme">
-    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
-    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
-    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
-    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
-    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
-    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
-    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
-    <div class="item"><h4><img src="/images/generic-placeholder_user.jpg"></h4></div>
+
+
+    <div class="card" style="width: 18rem;">
+        <a href="/gallery/_DSC0388.html" class="stretched-link text-decoration-none">
+            <!-- 16:9 ratio wrapper -->
+            <div class="ratio ratio-16x9">
+                <img src="/images/_temps/Gallery/_DSC0388.jpg" class="card-img-top object-fit-cover" alt="Card image">
+            </div>
+
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                </p>
+            </div>
+        </a>
+    </div>
+
+
+    <div class="card" style="width: 18rem;">
+        <a href="/gallery/_DSC0388.html" class="stretched-link text-decoration-none">
+            <!-- 16:9 ratio wrapper -->
+            <div class="ratio ratio-16x9">
+                <img src="/images/_temps/Gallery/_DSC0388.jpg" class="card-img-top object-fit-cover" alt="Card image">
+            </div>
+
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                </p>
+            </div>
+        </a>
+    </div>
 
 </div>
+
+
+
+
+
+
 
 <script>
     $('.owl-carousel').owlCarousel({
         loop: true,
-        margin: 10,
+        margin: 250,
         nav: true,
         responsive: {
             0: {
